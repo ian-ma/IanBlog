cffe31f029fbde7b1daf9bcb65529bbd1c67af22
Ian Ma <ian.yiming.ma@gmail.com>
2020-01-30 14:50:41 -0600

    set nginx server and public dir

--- config/logging.php
+++ config/logging.php
@@ -38,12 +38,12 @@ return [
         'stack' => [
             'driver' => 'stack',
             'channels' => ['single'],
-            'ignore_exceptions' => false,
+//            'ignore_exceptions' => false,
         ],
 
         'single' => [
-            'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
+            'driver' => 'errorlog',
+//            'path' => storage_path('logs/laravel.log'),
             'level' => 'debug',
         ],
 
